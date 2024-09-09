90b39702da1c67e3c9d19e7bb678ba071256bd08
Joel Wang <joelwangusa@gmail.com>
2024-09-09 15:42:13 -0700

    add more highlights into the homepage

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// Mobile navigation toggle
-const navToggle = document.getElementById('nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
